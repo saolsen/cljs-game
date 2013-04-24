2d572913a0329efbd6c7c97e9e9c4cff3b505ad1
Stephen Olsen <steve.olsen@gmail.com>
2013-04-24 15:55:59 -0400

    lein2

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject game "0.1.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
   :description "a game about time travel"
   :url "http://game.steveolsen.us"
   :dependencies [[org.clojure/clojure "1.5.0"]
